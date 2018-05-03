cb1f62e29c0758640c1bd0e15a3dbf8687d290c4
dayladavid <38049546+dayladavid@users.noreply.github.com>
2018-05-02 23:13:08 -0700

    sql

---
+++ sql php cmpe131.sql
@@ -0,0 +1,30 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "micemice123";
+$dbname = "myDB";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+// sql table
+create table userinfo
+(
+    user_id int PRIMARY KEY,
+    username varchar(20) NOT NULL,
+    email varchar(255) NOT NULL,
+    password varchar(32) NOT NULL,
+    create_time timestamp
+);
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table 'userinfo' created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
+$conn->close();
+?>
